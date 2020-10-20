06c72e1a31da122e69a0acc2c8256208d29d941f
shehanp12 <shehanp068@gmail.com>
2020-10-21 01:39:25 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import React from 'react';
 const App =() => {
   return (
     <div className="App">
-      <header className="App-header">
-      Quiz
-      </header>
+     Quiz
     </div>
   );
 }
