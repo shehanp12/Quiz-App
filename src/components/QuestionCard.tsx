9b79f55a8ca5960c793388a40401f6e43521dd49
shehanp12 <shehanp068@gmail.com>
2020-10-21 02:00:14 +0530

    Props

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,14 @@
 import React  from "react"
 
+type Props = {
+    question:string;
+    answer: string[];
+    callback:any;
+    userAnswer:string;
+    questionNr:number;
+    totalQuestions:number;
+}
+
 const QuestionCard = () => (
     <div>Question Card</div>
 );
