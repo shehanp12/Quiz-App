f14c2d2fd82213c1844f3e20ab97809ea86da7dd
shehanp12 <shehanp068@gmail.com>
2020-10-21 11:51:55 +0530

    pros

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -2,25 +2,34 @@ import React  from "react"
 
 type Props = {
     question:string;
-    answer: string[];
+    answers: string[];
     callback:any;
-    userAnswer:string;
+    userAnswer:boolean;
     questionNr:number;
     totalQuestions:number;
 }
 
 const QuestionCard : React.FC<Props>= ({
                                            question,
-                                           answer,
+                                           answers,
                                            callback,
                                            userAnswer,
                                            questionNr,
                                            totalQuestions}) => (
               <div>
                   <p className="number">
-
+                      Question:{questionNr}/{totalQuestions}
                   </p>
                   <p dangerouslySetInnerHTML={{__html:question}}/>
+                  <div>
+                      {answers.map(answer =>(
+                          <div>
+                              <button disabled={userAnswer} onClick={callback}>
+                                  <span dangerouslySetInnerHTML={{__html: answer}}/>
+                              </button>
+                          </div>
+                      ))}
+                  </div>
               </div>
 );
 
