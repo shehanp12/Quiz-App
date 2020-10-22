029f2bc20efc6151bec05c6286ee867323110953
shehanp12 <shehanp068@gmail.com>
2020-10-22 10:47:14 +0530

    api description

---
+++ src/API.ts
@@ -0,0 +1,27 @@
+export type Question = {
+    category : string;
+    correct_answer:string;
+    difficulty:string;
+    incorrect_answer:string[];
+    question:string;
+    type:string;
+
+}
+
+export type QuestionState = Question &  {answers:string[]};
+
+
+export enum Difficulty {
+    EASY = "easy",
+    MEDIUM = "medium",
+    HARD = "hard"
+}
+
+export const  fetchQuizQuestions = async (amount :number,difficulty:Difficulty)=>{
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+
+    const data = await (await fetch(endpoint)).json();
+    console.log(data);
+
+}
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
-import QuestionCard from "./components/QuestionCard";
+import React, {useState} from 'react';
+import {Difficulty, fetchQuizQuestions} from './API';
+//Components
+//types
 
 const App =() => {
+
+    const TOTAL_QUESTIONS = 10;
+
+    console.log(fetchQuizQuestions(TOTAL_QUESTIONS,Difficulty.EASY))
+
+
+    const [loading,setLoading] = useState(false);
+    const [questions,setQuestions] = useState([]);
+    const [number,setNumber] = useState(0);
+    const [userAnswers,setUserAnswers] = useState([]);
+    const  [score,setScore] = useState(0);
+    const [gameOver, setGameOver] = useState(true);
+
+
+
+
 
     const startTrivia = async () => {
 
@@ -25,7 +43,15 @@ const App =() => {
      </button>
         <p className="score">Score: </p>
         <p>Loading Questions..</p>
-        <QuestionCard question={} answer={} callback={} userAnswer={} questionNr={} totalQuestions={}></QuestionCard>
+        {/*<QuestionCard*/}
+        {/*    questionNr={number +1}*/}
+        {/*    totalQuestions={TOTAL_QUESTIONS}*/}
+        {/*    question={questions[number].question}*/}
+        {/*    answers={questions[number].answers}*/}
+        {/*    userAnswer={userAnswers ? userAnswers[number]:undefined}*/}
+        {/*    callback={checkAnswer}/>*/}
+
+
         <button className="next" onClick={nextQuestion}>
             Next Question
         </button>
